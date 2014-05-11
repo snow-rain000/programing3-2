09b6e98c71a0a44bf070106ebf958f6949da013f
snow-rain000 <e125730@ie.u-ryukyu.ac.jp>
2014-05-12 04:27:50 +0900

    add by ~~

--- myapp.rb
+++ myapp.rb
@@ -49,6 +49,7 @@ end
 
 get '/top' do
    @comments = Comment.order("id desc").all
+   @write_user = session[:AcountName]
    erb :bbs
 end
 
@@ -86,7 +87,7 @@ end
 # Twitter Request authentication
 get '/twitter/auth' do
   # Appointname callback URL
-  callback_url = "http://127.0.1:4567/twitter/callback"
+  callback_url = "http://133.13.60.165:4567/twitter/callback"
   request_token = oauth_consumer.get_request_token(oauth_callback: callback_url)
  
   # セッションにトークンを保存
