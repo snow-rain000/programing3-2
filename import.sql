18f4ea436a641cd3cbd7345beed527ea097e6f55
snow-rain000 <e125730@ie.u-ryukyu.ac.jp>
2014-05-14 06:59:22 +0900

    change import.sql

--- import.sql
+++ import.sql
@@ -1,6 +1,11 @@
 create table comments (
     id integer primary key,
     body text,
+    username text
+);
+
+create table followers (
+    id integer primary key,
     username text,
     follower text
 );
